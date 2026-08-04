6806c94a2724637770fd01d538362f25b9febf3a
kyosuke36 <futianjingyou575@gmail.com>
2026-08-04 07:55:21 +0000

    バックの作成

--- questions.py
+++ questions.py
@@ -1,3 +1,56 @@
-"""クイズで使用する問題データ。"""
+import random
+# """クイズで使用する問題データ。"""
 
-QUESTIONS = []
+futoccho_questions = [
+    {
+        "question": "問題文1",
+        "answer": "ふとっちょ",
+        "comment": "解説1"
+    },
+    {
+        "question": "問題文2",
+        "answer": "ふとっちょ",
+        "comment": "解説2"
+    },
+    {
+        "question": "問題文3",
+        "answer": "ふとっちょ",
+        "comment": "解説3"
+    }
+]
+
+macho_questions = [
+    {
+        "question": "問題文1",
+        "answer": "マッチョ",
+        "comment": "解説1"
+    },
+    {
+        "question": "問題文2",
+        "answer": "マッチョ",
+        "comment": "解説2"
+    },
+    {
+        "question": "問題文3",
+        "answer": "マッチョ",
+        "comment": "解説3"
+    }
+]
+
+def create_quiz():
+    futoccho = random.sample(futoccho_questions, 3)
+
+    macho = random.sample(macho_questions, 3)
+
+    quiz_list = futoccho + macho
+
+    random.shuffle(quiz_list)
+
+    return quiz_list
+
+def check_answer(question, user_answer):
+
+    if user_answer == question["answer"]:
+        return True
+
+    return False
